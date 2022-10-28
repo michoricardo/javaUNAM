2aa2002e57d8dfee3f647fd4fe9dc4b15dbddbd9
Michoricardo <ricardog94@hotmail.com>
2022-10-28 01:37:05 -0500

    Update estaturas.java

--- estaturas.java
+++ estaturas.java
@@ -1,43 +1,50 @@
 /* Dadas las lecturas de la estatura de 10 personas, hacer un programa en Java para determinar
 la media y la desviación estándar de las estaturas y determinar la estatura menor a la media y
 la estatura mayor a la media.
-
-
-Paso 1: calcular la media.
-Paso 2: calcular el cuadrado de la distancia a la media para cada dato.
-Paso 3: sumar los valores que resultaron del paso 2.
-Paso 4: dividir entre el número de datos.
-Paso 5: sacar la raíz cuadrada.
 */
-class Main {  
-  public static void main(String args[]) { 
-  int[] califs = {1, 2, 3, 4, 5, 6};
-         
-        double suma = 0;
-        double suma2 = 0;
-         
-        //suma
-        int i=0;
-        while(i < califs.length) {
-            suma += califs[i];
-            i++;
-        }
-         
-        //media
-        double media = (suma / califs.length); 
-        
-        //cuadrado de diferencia de media p c/estatura
-        int y=0;
-        while(y < califs.length) {
-            suma2 += Math.pow((califs[y]-media),2);
-            y++;
-        }
-        System.out.println("suma2 : "+suma2);
-        double de= Math.sqrt(suma2/califs.length);
-    
-         
-        System.out.println("Promedio : "+media);
-        System.out.println("Desviación estándar : "+de);
 
-  } 
+
+class Main {
+  public static void main(String args[])  {
+
+    double[] estaturas = { 1.65, 1.50, 1.55, 1.65, 1.70, 1.75, 1.80, 1.45, 1.85, 1.95};
+
+
+    double suma = 0;
+    double suma2 = 0;
+
+    // suma
+    int i = 0;
+    while (i < estaturas.length) {
+      suma += estaturas[i];
+      i++;
+    }
+
+    // media
+    double media = (suma / estaturas.length);
+
+    // cuadrado de diferencia de media p c/estatura
+    int y = 0;
+    while (y < estaturas.length) {
+      suma2 += Math.pow((estaturas[y] - media), 2);
+      y++;
+    }
+    //System.out.println("suma2 : " + suma2);
+    double de = Math.sqrt(suma2 / estaturas.length);
+
+    System.out.println("Estatura promedio : " + media);
+    System.out.println("Desviación estándar : " + de);
+    System.out.println("Las siguientes estaturas son menores a la media:");
+  for (int j = 0; j < estaturas.length; j++) {
+    if (estaturas[j] < media){
+      System.out.println(estaturas[j]);
+    }
+    }
+    System.out.println("Las siguientes estaturas son mayores a la media:");
+  for (int j = 0; j < estaturas.length; j++) {
+    if (estaturas[j] > media){
+      System.out.println(estaturas[j]);
+    }
+  }
+}
 }
