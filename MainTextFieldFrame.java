a7c9c61cfcd894a8cbd67014528292cb58978863
Michoricardo <ricardog94@hotmail.com>
2022-12-06 13:33:16 -0600

    Create MainTextFieldFrame.java

---
+++ MainTextFieldFrame.java
@@ -0,0 +1,7 @@
+//Archivo que va junto al de TextFieldFrame.java
+class Main {
+public static void main(String[] args) {
+System.out.println("Creando elementos del JFrame");
+new TextFieldFrame();
+}
+}
